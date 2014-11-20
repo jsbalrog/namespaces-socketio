7ee2f5747b43046b8325e6ca72477ac3df2797c5
jsbalrog <edwardtjenkins@gmail.com>
2014-11-20 16:19:44 -0700

    Fixed privatePing. Added comments.

--- namespace.js
+++ namespace.js
@@ -3,14 +3,17 @@ var io = require('socket.io').listen(4000);
 io.sockets.on('connection', function(socket) {
 	socket.on('join', function(data) {
 		socket.username = data.username;
-		socket.broadcast.emit('join', { username: data.username, socket: socket.id  });
+		socket.broadcast.emit('join', { username: data.username, socket: socket.id });
 	});
-	
+
 	socket.on('ping', function() {
 		socket.broadcast.emit('ping', { username: socket.username });
 	});
-	
+
 	socket.on('privatePing', function(data) {
-		io.sockets.connected[data.socket].emit('ping', { username: socket.username, priv, true });
+		// io.sockets.connected has all the connected sockets.
+		//The data.socket object contains the socket id.
+		io.sockets.connected[data.socket].emit('ping',
+			{ username: socket.username, priv: true });
 	});
 });
